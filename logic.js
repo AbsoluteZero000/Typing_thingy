3e993a012649ebd683d996aa7de1aa3e23fe01ea
Omar Brikaa <brikaaomar@gmail.com>
2022-02-25 21:45:13 +0200

    Do type racing logic

--- logic.js
+++ logic.js
@@ -1,8 +1,39 @@
-(() => {
-  const elements = {
-    start_button: document.getElemenyById('start-button')
-  }
+(async () => {
+    const elements = {
+        text_demo: document.getElementById('text-demo'),
+        text_area: document.getElementById('typing-place')
+    };
+    const QUOTES_URL = 'http://quotes.stormconsultancy.co.uk/random.json';
+    let text;
 
-  start_button.addEventListener();
+    const get_text = async () => {
+        const data = await fetch(QUOTES_URL);
+        const data_json = await data.json();
+        const quote = data_json.quote;
+        return quote;
+    };
+
+    const check = (text_to_check, actual_text) => {
+        return text_to_check === actual_text;
+    };
+
+    const main = () => {
+        elements.text_area.removeEventListener('input', main);
+        const start_time = Date.now();
+        elements.text_area.addEventListener('input', (e) => {
+            if (check(e.target.value, text)) {
+                const end_time = Date.now();
+                const milliseconds_delta = end_time - start_time;
+                const minutes_delta = milliseconds_delta / (1000 * 60);
+                const no_words = text.split(' ').length;
+                const wpm = no_words / minutes_delta;
+                console.log(wpm);
+            }
+        });
+    };
+
+    text = await get_text();
+    elements.text_demo.innerHTML = text;
+    elements.text_area.value = '';
+    elements.text_area.addEventListener('input', main);
 })();
-
